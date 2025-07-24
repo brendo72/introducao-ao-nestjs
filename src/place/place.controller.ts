51309f66b7b007f718389582ab2e12c7f16f30b1
Jonas Fortes <jonascforte@hotmail.com>
2025-07-24 18:14:12 -0300

    pagineted places

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -1,22 +1,50 @@
-import { BadRequestException, Body, Controller, Delete, Put, Post, Get, UploadedFiles, UseInterceptors, Param } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Put,
+  Post,
+  Get,
+  UploadedFiles,
+  UseInterceptors,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { PlaceService } from './place.service';
 import { CreatePlaceDto } from './dto/create-place.dto';
-import { File as MulterFile } from 'multer'
+import { File as MulterFile } from 'multer';
 import { CloudinaryService } from './cloudinary.service';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
-import { ApiConsumes, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import {
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiQuery,
+} from '@nestjs/swagger';
 import { UpdatePlaceDto } from './dto/update-place.dto';
 
 @Controller('place')
 export class PlaceController {
   constructor(
     private readonly placeService: PlaceService,
-    private cloudinaryService: CloudinaryService
-  ) { }
+    private cloudinaryService: CloudinaryService,
+  ) {}
 
   @Get()
-  findAll(){
+  findAll() {
     return this.placeService.findAll();
+  }
+
+  @Get('pagineted')
+  @ApiOperation({summary: "Listar locais paginados"})
+  @ApiQuery({name: 'page', required: false, type: Number, example: 1})
+  @ApiQuery({name: 'limit', required: false, type: Number, example: 10})
+  async findPaginated(@Query('page') page = 1, @Query('limit') limit = 10) {
+    const parsePage = Math.max(1, Number(page))
+    const parseLimit = Math.min(50, Math.max(1, Number(limit))) // Limmite de segurança
+    return this.placeService.findPaginated(parsePage, parseLimit)
   }
 
   @Post()
@@ -39,33 +67,31 @@ export class PlaceController {
           type: 'array',
           items: {
             type: 'string',
-            format: 'binary'
+            format: 'binary',
           },
-          description: 'Máximo de 3 imagens'
-        }
-      }
-    }
+          description: 'Máximo de 3 imagens',
+        },
+      },
+    },
   })
   async createPlace(
     @Body() data: CreatePlaceDto,
-    @UploadedFiles() files: { images?: MulterFile[] }
+    @UploadedFiles() files: { images?: MulterFile[] },
   ) {
-
     if (!files.images || files.images.length === 0) {
-      throw new BadRequestException('Pelo menos uma imagem deve ser enviada')
+      throw new BadRequestException('Pelo menos uma imagem deve ser enviada');
     }
 
     const imagesUrls = await Promise.all(
-      files.images.map(
-        (file) => this.cloudinaryService.uploadImage(file.buffer)
-      )
-    )
+      files.images.map((file) =>
+        this.cloudinaryService.uploadImage(file.buffer),
+      ),
+    );
 
     return this.placeService.create({
       ...data,
-      images: imagesUrls
-    })
-
+      images: imagesUrls,
+    });
   }
 
   @Put(':id')
@@ -88,31 +114,28 @@ export class PlaceController {
           type: 'array',
           items: {
             type: 'string',
-            format: 'binary'
+            format: 'binary',
           },
-          description: 'As novas imagens substituem as anteriores (Máximo 3 imagens)'
-        }
-      }
-    }
+          description:
+            'As novas imagens substituem as anteriores (Máximo 3 imagens)',
+        },
+      },
+    },
   })
   async updatePlace(
     @Param('id') id: string,
     @Body() data: UpdatePlaceDto,
-    @UploadedFiles() files: { images?: MulterFile[] }
-  ){
-    const newImages = files.images?.map(img => img.buffer)
+    @UploadedFiles() files: { images?: MulterFile[] },
+  ) {
+    const newImages = files.images?.map((img) => img.buffer);
 
-    return this.placeService.update(id, data, newImages)
-
+    return this.placeService.update(id, data, newImages);
   }
 
   @Delete(':id')
-  @ApiOperation({summary: 'Deletar um local e suas imagens no Cloudinary'})
-  @ApiResponse({status:200, description: 'Local deletado com sucesso!'})
-  async deletePlace(@Param('id') id:string){
-    return this.placeService.delete(id)
+  @ApiOperation({ summary: 'Deletar um local e suas imagens no Cloudinary' })
+  @ApiResponse({ status: 200, description: 'Local deletado com sucesso!' })
+  async deletePlace(@Param('id') id: string) {
+    return this.placeService.delete(id);
   }
-
 }
-
-
